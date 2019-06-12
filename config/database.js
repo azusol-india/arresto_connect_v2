721dc612621191af8536478ba6de54fc0f792da7
101sachin.tyagi@gmail.com <101sachin.tyagi@gmail.com>
2019-06-12 19:08:37 +0530

    conditional connetion

--- config/database.js
+++ config/database.js
@@ -11,15 +11,29 @@ mongoose.connect(dbString, err => {
 });
  */
 
+const os = require("os");
 const mongoose = require("mongoose");
+const host = os.hostname();
+console.log(host);
+
 const dbConnection = function() {
+  let conString = "";
+  if (host == "DESKTOP-5ESDTJT") {
+    conString = "mongodb://localhost/playground";
+  } else {
+    conString =
+      "mongodb+srv://azusoluser:password2019@cluster0-pydbi.mongodb.net/test?retryWrites=true&w=majority";
+  }
+  console.log(conString);
   mongoose
-    .connect("mongodb://localhost/playground")
+    .connect(conString)
     .then(() => {
+      console.log(conString);
       console.log("db connected success==============");
     })
     .catch(err => {
       console.log("db connected fail !!!!!!!!!!");
     });
 };
+
 module.exports = dbConnection;
