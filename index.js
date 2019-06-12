4f61e24766199b14bd03e82a6417e8548b04d02f
101sachin.tyagi@gmail.com <101sachin.tyagi@gmail.com>
2019-06-12 17:54:08 +0530

    database file added to the code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express"); // return function
 const app = express();
-
 app.use(express.json());
+
+require("./config/database")();
 
 //Appling the middle ware
 const userRouter = require("./routers/users");
+const customerRouetr = require("./routers/customers");
+
 app.use("/api/users", userRouter);
+app.use("/api/customers", customerRouetr);
 
 app.get("/", (req, res) => {
   res.send("Node server running successfully !!");
@@ -17,8 +21,8 @@ app.get("/api", (req, res) => {
   res.send("API running successfully !!");
 }); */
 
+//console.log(process.env);
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, server => {
   console.log("server listening at port : " + PORT);
 });
--- models/user.js
+++ models/user.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
+//console.log(config.get("dbString"));
+
 mongoose
   .connect(
-    "mongodb+srv://azusoluser:password2019@cluster0-pydbi.mongodb.net/test?retryWrites=true&w=majority",
+    //"mongodb+srv://azusoluser:password2019@cluster0-pydbi.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb://localhost/playground",
     { useNewUrlParser: true }
-    //"mongodb://localhost/playground"
   )
   .then(db => {
-    console.log("mongo connected");
+    console.log("mongo connected===========");
   })
   .catch(err => {
     console.log(err.message);
     console.log("mongo connection error");
   });
+
+mongoose.testProperty = "sachin tyagi";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,17 +32,41 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  },
+  profile: {
+    type: new mongoose.Schema({
+      fname: {
+        type: String
+      },
+      lname: String,
+      imeiNo: String,
+      companyName: String,
+      companyAddress: String,
+      address: {
+        type: new mongoose.Schema({
+          country: String,
+          state: String,
+          city: String,
+          line1: String,
+          line2: String,
+          pincode: String
+        }),
+        required: true
+      }
+    }),
+    required: true,
+    required: true
   }
 });
 
 // compile the schema into model
 const User = mongoose.model("User", userSchema);
 
-/* async function getUsers() {
+async function getUsers() {
   const users = await User.find({});
   console.log(users);
 }
-getUsers();
- */
+//getUsers();
+console.log(mongoose.testProperty);
 
 module.exports = User;
