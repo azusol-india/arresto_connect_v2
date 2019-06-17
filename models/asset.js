b08069405cada4714d0bc91c6aa68e429316d682
101sachin.tyagi@gmail.com <101sachin.tyagi@gmail.com>
2019-06-17 18:08:35 +0530

    new changes

---
+++ models/asset.js
@@ -0,0 +1,88 @@
+const { Sequelize, sequelize, Model } = require("../sequelize");
+
+class Asset extends Model {}
+
+Asset.init(
+  {
+    id: {
+      type: Sequelize.STRING,
+      field: "component_id",
+      primaryKey: true
+    },
+    clientFk: {
+      type: Sequelize.STRING,
+      field: "component_client_fk"
+    },
+    assetCode: {
+      type: Sequelize.STRING,
+      field: "component_code",
+      unique: true
+    },
+    description: {
+      type: Sequelize.STRING,
+      field: "component_description"
+    },
+    subAssets: {
+      type: Sequelize.STRING,
+      field: "component_sub_assets"
+    },
+    image: {
+      type: Sequelize.STRING,
+      field: "component_imagepath"
+    },
+    uom: {
+      type: Sequelize.STRING,
+      field: "component_uom"
+    },
+    inspectionType: {
+      type: Sequelize.STRING,
+      field: "component_inspectiontype"
+    },
+    expectResults: {
+      type: Sequelize.STRING,
+      field: "component_expectedresult"
+    },
+    observations: {
+      type: Sequelize.STRING,
+      field: "component_observation"
+    },
+    isRepairable: {
+      type: Sequelize.STRING,
+      field: "component_repair"
+    },
+    geo: {
+      type: Sequelize.STRING,
+      field: "component_geo_fancing"
+    },
+    status: {
+      type: Sequelize.ENUM,
+      values: ["active", "inactive"],
+      field: "status"
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    tableName: "components",
+    timestamps: false,
+    underscored: true
+  }
+);
+
+function getData() {
+  Asset.findAndCountAll({
+    //limit: 10
+  })
+    .then(asset => {
+      // get the instance of sequelize
+      //asset = asset.get({ plain: true });
+      console.log(JSON.stringify(asset));
+      //return asset;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
+
+//getData();
+module.exports.Asset = Asset;
--- models/masterData.js
+++ models/masterData.js
@@ -1,120 +1,100 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+const { Sequelize, sequelize, Model } = require("../sequelize");
+const { Asset } = require("./asset");
 
-const mastreDataSchema = new mongoose.Schema({
-  clentId: {
-    type: String
+console.log(Asset.rawAttributes);
+
+class MasterData extends Model {}
+
+MasterData.init(
+  {
+    id: {
+      type: Sequelize.STRING,
+      field: "mdata_id",
+      primaryKey: true
+    },
+    clientFk: {
+      type: Sequelize.STRING,
+      field: "mdata_client_fk"
+    },
+    jobcard: {
+      type: Sequelize.STRING,
+      field: "mdata_jobcard"
+    },
+    sms: {
+      type: Sequelize.STRING,
+      field: "mdata_sms"
+    },
+    batchNo: {
+      type: Sequelize.STRING,
+      field: "mdata_client_fk"
+    },
+    serialNo: {
+      type: Sequelize.STRING,
+      field: "mdata_serial"
+    },
+    rfid: {
+      type: Sequelize.STRING,
+      field: "mdata_rfid"
+    },
+    uin: {
+      type: Sequelize.STRING,
+      field: "mdata_uin"
+    },
+    client: {
+      type: Sequelize.STRING,
+      field: "mdata_client"
+    },
+    poNo: {
+      type: Sequelize.STRING,
+      field: "mdata_po"
+    },
+    series: {
+      type: Sequelize.STRING,
+      field: "mdata_item_series"
+    },
+    asset: {
+      // defining the foreign key ===============
+      type: Sequelize.STRING,
+      references: {
+        model: Asset,
+        key: "assetCode"
+      },
+      field: "mdata_asset"
+    },
+    status: {
+      type: Sequelize.ENUM,
+      values: ["active", "inactive"],
+      field: "status"
+    }
   },
-  mdataType: {
-    type: String,
-    required: true
-  },
-  jobcard: {
-    type: String,
-    required: true
-  },
-  sms: {
-    type: String,
-    required: true
-  },
-  batchNo: {
-    type: String,
-    required: true
-  },
-  serialNo: {
-    type: String,
-    required: true
-  },
-  rfid: {
-    type: String,
-    required: true
-  },
-  barcode: {
-    type: String,
-    required: true
-  },
-  uin: {
-    type: String,
-    required: true
-  },
-  client: {
-    type: String,
-    required: true
-  },
-  dealer: {
-    type: String,
-    required: true
-  },
-  poNo: {
-    type: String,
-    required: true
-  },
-  series: {
-    type: String,
-    required: true
-  },
-  asset: {
-    type: String,
-    required: true
-  },
-  materialInvoiceNo: {
-    type: String,
-    required: true
-  },
-  materialInvoiceDate: {
-    type: String,
-    required: true
-  },
-  dateOfFirstUse: {
-    type: String,
-    required: true
-  },
-  dateOfInspection: {
-    type: String,
-    required: true
-  },
-  inspectionDueDate: {
-    type: String,
-    required: true
-  },
-  dateOfPdm: {
-    type: String,
-    required: true
-  },
-  pdmDueDate: {
-    type: String,
-    required: true
+  {
+    sequelize,
+    freezeTableName: true,
+    tableName: "master_data",
+    timestamps: false
   }
-});
+);
 
-function validateMasterData(user) {
-  const dataSchema = Joi.object({
-    mdataType: Joi.any().required(),
-    jobcard: Joi.string().required(),
-    sms: Joi.string().required(),
-    batchNo: Joi.string().required(),
-    serialNo: Joi.string().required(),
-    rfid: Joi.string().required(),
-    uin: Joi.string().required(),
-    barcode: Joi.string().required(),
-    uin: Joi.string().required(),
-    client: Joi.string().required(),
-    dealer: Joi.string().required(),
-    poNo: Joi.string().required(),
-    asset: Joi.string().required(),
-    series: Joi.string().required(),
-    materialInvoiceNo: Joi.string().required(),
-    materialInvoiceDate: Joi.string().required(),
-    dateOfFirstUse: Joi.string().required(),
-    dateOfInspection: Joi.string().required(),
-    inspectionDueDate: Joi.string().required(),
-    dateOfPdm: Joi.string().required(),
-    pdmDueDate: Joi.string().required()
+Asset.hasOne(MasterData);
+
+//MasterData.belongsTo(Asset);
+
+console.log("===========================");
+
+//module.exports.MasterData = MasterData;
+MasterData.findOne({
+  /*   include: [
+    {
+      model: Asset,
+      where: { assetCode: Sequelize.col(mdata.asset) }
+    }
+  ] */
+})
+  .then(mdata => {
+    // get the instance of sequelize
+    mdata = mdata.get({ plain: true });
+    console.log(mdata);
+  })
+  .catch(err => {
+    console.log(err.message);
   });
-  return Joi.validate(user, dataSchema);
-}
-
-const MasterData = mongoose.model("MasterData", mastreDataSchema);
-
-module.exports.MasterData = MasterData;
-module.exports.validateMasterData = validateMasterData;
