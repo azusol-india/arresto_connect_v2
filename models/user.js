e78e1943bc003eb59fabfe500e3ee048fae610db
101sachin.tyagi@gmail.com <101sachin.tyagi@gmail.com>
2019-06-19 18:28:44 +0530

    new changes for mangoose/mongo

--- models/user.js
+++ models/user.js
@@ -18,7 +18,44 @@ const Joi = require("joi");
   });
  */
 
+const addressSchema = new mongoose.Schema({
+  country: String,
+  state: String,
+  city: String,
+  line1: String,
+  line2: String,
+  pincode: String
+});
+const profileSchema = new mongoose.Schema({
+  fname: String,
+  lname: String,
+  imeiNo: String,
+  companyName: String,
+  companyAddress: String,
+  address: addressSchema
+});
+
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 255,
+    required: 1
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profile: profileSchema,
+  qrcode: String
+});
+
+/* const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 3,
@@ -55,16 +92,48 @@ const userSchema = new mongoose.Schema({
     }),
     required: true
   }
-});
+}); */
 
 // compile the schema into model
 const User = mongoose.model("User", userSchema);
-
 async function getUsers() {
   const users = await User.find({});
   console.log(users);
 }
 //getUsers();
-console.log(mongoose.testProperty);
+//console.log(mongoose.testProperty);
 
-module.exports = User;
+function validateUser(user) {
+  const addressSchema = Joi.object({
+    country: Joi.string().required(),
+    state: Joi.string(),
+    city: Joi.string(),
+    line1: Joi.string(),
+    line2: Joi.string(),
+    pincode: Joi.string().required()
+  });
+  const profileSchema = Joi.object().keys({
+    fname: Joi.string(),
+    lname: Joi.string(),
+    companyName: Joi.string(),
+    companyAddress: Joi.string(),
+    address: addressSchema
+  });
+
+  const userSchema = Joi.object().keys({
+    name: Joi.string()
+      .min(3)
+      .required(),
+    email: Joi.string().required(),
+    password: Joi.string()
+      .min(4)
+      .required(),
+    profile: profileSchema.required(),
+    qrcode: Joi.string().required()
+  });
+
+  return Joi.validate(user, userSchema);
+}
+
+module.exports.User = User;
+module.exports.validateUser = validateUser;
